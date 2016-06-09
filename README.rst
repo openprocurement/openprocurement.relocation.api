4cf66e84a928d06738c44bd87923f5e0f43f0b03
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-06-09 17:58:49 +0300

    initial add

---
+++ README.rst
@@ -0,0 +1,6 @@
+openprocurement.relocation.api
+==============================
+
+Ownership transfer API interface to OpenProcurement database
+
+Documentation for this package is available at http://relocation.api-docs.openprocurement.org/en/latest/overview.html
---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+requires = [
+    'couchdb',
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+]
+
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
+entry_points = {
+    'openprocurement.api.plugins': [
+        'relocation = openprocurement.relocation.api:includeme'
+    ]
+}
+
+setup(name='openprocurement.relocation.api',
+      version=version,
+      description="",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+
+      url='https://github.com/openprocurement/openprocurement.relocation.api',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['openprocurement', 'openprocurement.relocation'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'test': test_requires, 'docs': docs_requires},
+      entry_points=entry_points,
+      )
