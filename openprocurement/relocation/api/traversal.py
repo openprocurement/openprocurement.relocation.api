8abb75a7c134211d2745b1015807385ad7239e3a
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-06-10 19:51:36 +0300

    added traverse

---
+++ openprocurement/relocation/api/traversal.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openprocurement.api.traversal import Root as BaseRoot
+
+from pyramid.security import ALL_PERMISSIONS, Allow  # , Everyone
+
+
+class Root(BaseRoot):
+
+    __acl__ = [
+        # (Allow, Everyone, 'view_transfer'),
+        (Allow, 'g:brokers', 'view_transfer'),
+        (Allow, 'g:brokers', 'create_transfer'),
+        (Allow, 'g:admins', ALL_PERMISSIONS),
+    ]
+
+
+def factory(request):
+    request.validated['transfer_src'] = {}
+    root = Root(request)
+    if not request.matchdict or not request.matchdict.get('transfer_id'):
+        return root
+    request.validated['transfer_id'] = request.matchdict['transfer_id']
+    transfer = request.transfer
+    transfer.__parent__ = root
+    request.validated['transfer'] = transfer
+    if request.method != 'GET':
+        request.validated['transfer_src'] = transfer.serialize('plain')
+    request.validated['id'] = request.matchdict['transfer_id']
+    return transfer
