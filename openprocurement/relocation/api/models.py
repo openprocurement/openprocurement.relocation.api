2d7b6b7e7a79ddb494ecb6449dd78f24f0a970ba
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-06-13 16:55:55 +0300

    update roles, add accrediration level definition

--- openprocurement/relocation/api/models.py
+++ openprocurement/relocation/api/models.py
@@ -9,7 +9,7 @@ from schematics.types.compound import ModelType, DictType
 from schematics.types.serializable import serializable
 from schematics.exceptions import ValidationError
 from schematics.transforms import whitelist, blacklist
-from openprocurement.api.models import Model, plain_role
+from openprocurement.api.models import Model, plain_role, schematics_default_role
 
 
 class Transfer(SchematicsDocument, Model):
@@ -17,14 +17,17 @@ class Transfer(SchematicsDocument, Model):
     class Options:
         roles = {
             'plain': plain_role,
+            'default': schematics_default_role,
             'create': whitelist(),
-            'view': whitelist('id', 'usedFor'),
+            'view': whitelist('id', 'doc_id', 'usedFor'),
         }
 
-    owner = StringType(required=True, min_length=1)
+    owner = StringType(min_length=1)
     access_token = StringType(min_length=1)
     transfer_token = StringType(min_length=1)
     usedFor = StringType()
+
+    create_accreditation = 3
 
     def __repr__(self):
         return '<%s:%r@%r>' % (type(self).__name__, self.id, self.rev)
