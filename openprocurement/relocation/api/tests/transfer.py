72c859f737fdf0ba4db8ebe6d1b9830146da32df
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-06-13 17:04:59 +0300

    add transfer route tests

--- openprocurement/relocation/api/tests/transfer.py
+++ openprocurement/relocation/api/tests/transfer.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import unittest
+from uuid import uuid4
+from copy import deepcopy
 
 from openprocurement.relocation.api.models import Transfer
 from openprocurement.relocation.api.tests.base import BaseWebTest
+
+test_transfer_data = {}
 
 
 class TrensferTest(BaseWebTest):
@@ -31,9 +35,115 @@ class TrensferTest(BaseWebTest):
         u.delete_instance(self.db)
 
 
+class TransferResourceTest(BaseWebTest):
+    """ /transfers resource test """
+
+    def test_get_transfer(self):
+        response = self.app.get('/transfers', status=405)
+        self.assertEqual(response.status, '405 Method Not Allowed')
+
+        response = self.app.post_json('/transfers', {'data': test_transfer_data})
+        self.assertEqual(response.status, '201 Created')
+        transfer = response.json['data']
+        self.assertIn('id', transfer)
+
+        response = self.app.get('/transfers/{}'.format(transfer['id']))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['data'], transfer)
+
+        response = self.app.get('/transfers/{}?opt_jsonp=callback'.format(transfer['id']))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/javascript')
+        self.assertIn('callback({"data": {"', response.body)
+
+        response = self.app.get('/transfers/{}?opt_pretty=1'.format(transfer['id']))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertIn('{\n    "data": {\n        "', response.body)
+
+    def test_not_found(self):
+        response = self.app.post_json('/transfers', {'data': test_transfer_data})
+        self.assertEqual(response.status, '201 Created')
+        transfer = response.json['data']
+
+        response = self.app.get('/transfers/{}'.format("1234" * 8), status=404)
+        self.assertEqual(response.status, '404 Not Found')
+
+        from openprocurement.api.tests.base import test_tender_data
+        orig_auth = self.app.authorization
+        self.app.authorization = ('Basic', ('broker1', ''))
+        response = self.app.post_json('/tenders', {"data": test_tender_data})
+        self.assertEqual(response.status, '201 Created')
+        tender = response.json['data']
+        self.app.authorization = orig_auth
+
+        response = self.app.get('/transfers/{}'.format(tender['id']), status=404)
+        self.assertEqual(response.status, '404 Not Found')
+
+        data = deepcopy(test_transfer_data)
+        data['id'] = uuid4().hex
+        response = self.app.post_json('/transfers', {'data': data})
+        self.assertEqual(response.status, '201 Created')
+        transfer = response.json['data']
+        self.assertNotEqual(transfer['id'], data['id'])
+
+        response = self.app.get('/transfers/{}'.format(data['id']), status=404)
+        self.assertEqual(response.status, '404 Not Found')
+
+        response = self.app.get('/transfers/some_id', status=404)
+        self.assertEqual(response.status, '404 Not Found')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Not Found', u'location': u'url', u'name': u'transfer_id'}
+        ])
+
+        response = self.app.patch_json(
+            '/transfers/some_id', {'data': {}}, status=404)
+        self.assertEqual(response.status, '404 Not Found')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Not Found', u'location': u'url', u'name': u'transfer_id'}
+        ])
+
+    def test_create_transfer(self):
+        response = self.app.post_json('/transfers', {"data": test_transfer_data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        transfer = response.json['data']
+        self.assertNotIn('usedFor', transfer)
+        self.assertIn('token', response.json['access'])
+        self.assertIn('transfer', response.json['access'])
+
+        response = self.app.get('/transfers/{}'.format(transfer['id']))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(set(response.json['data']), set(transfer))
+        self.assertEqual(response.json['data'], transfer)
+
+        data = test_transfer_data
+        response = self.app.post_json('/transfers?opt_jsonp=callback', {"data": data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/javascript')
+        self.assertIn('callback({"', response.body)
+
+        response = self.app.post_json('/transfers?opt_pretty=1', {"data": data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertIn('{\n    "', response.body)
+
+        response = self.app.post_json('/transfers', {"data": data, "options": {"pretty": True}})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertIn('{\n    "', response.body)
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TrensferTest))
+    suite.addTest(unittest.makeSuite(TransferResourceTest))
     return suite
 
 
