5144833419bc41fbd85ec9d98daa15938bd0d255
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-06-17 19:06:54 +0300

    update tender ownership through transfer object

--- openprocurement/relocation/api/utils.py
+++ openprocurement/relocation/api/utils.py
@@ -63,3 +63,9 @@ def set_ownership(item, request, access_token=None, transfer_token=None):
     item.owner = request.authenticated_userid
     item.access_token = sha512(access_token).hexdigest()
     item.transfer_token = sha512(transfer_token).hexdigest()
+
+
+def update_ownership(tender, transfer):
+    tender.owner = transfer.owner
+    tender.owner_token = transfer.access_token
+    tender.transfer_token = transfer.transfer_token
