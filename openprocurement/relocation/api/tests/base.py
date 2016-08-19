aea73bcece463517aa83be91d8d5e1c265f4d3d2
Leits <leits@quintagroup.org>
2016-08-19 15:28:21 +0300

    Optimizing test setup

--- openprocurement/relocation/api/tests/base.py
+++ openprocurement/relocation/api/tests/base.py
@@ -4,8 +4,10 @@ import unittest
 import webtest
 from copy import deepcopy
 from datetime import datetime, timedelta
+from uuid import uuid4
 
 from openprocurement.api.utils import apply_data_patch
+from openprocurement.api.design import sync_design
 from openprocurement.api.tests.base import PrefixedRequestClass, test_tender_data
 now = datetime.now()
 
@@ -16,17 +18,41 @@ class BaseWebTest(unittest.TestCase):
     It setups the database before each test and delete it after.
     """
     initial_auth = ('Basic', ('broker', ''))
+    relative_to = os.path.dirname(__file__)
+
+    @classmethod
+    def setUpClass(cls):
+        while True:
+            try:
+                cls.app = webtest.TestApp("config:tests.ini", relative_to=cls.relative_to)
+            except:
+                pass
+            else:
+                break
+        cls.app.RequestClass = PrefixedRequestClass
+        cls.couchdb_server = cls.app.app.registry.couchdb_server
+        cls.db = cls.app.app.registry.db
+        cls.db_name = cls.db.name
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.couchdb_server.delete(cls.db_name)
+        except:
+            pass
 
     def setUp(self):
-        self.app = webtest.TestApp(
-            "config:tests.ini", relative_to=os.path.dirname(__file__))
-        self.app.RequestClass = PrefixedRequestClass
-        self.app.authorization = self.initial_auth
-        self.couchdb_server = self.app.app.registry.couchdb_server
+        self.db_name += uuid4().hex
+        self.couchdb_server.create(self.db_name)
+        db = self.couchdb_server[self.db_name]
+        sync_design(db)
+        self.app.app.registry.db = db
         self.db = self.app.app.registry.db
+        self.db_name = self.db.name
+        self.app.authorization = self.initial_auth
 
     def tearDown(self):
-        del self.couchdb_server[self.db.name]
+        self.couchdb_server.delete(self.db_name)
 
 
 class OwnershipWebTest(BaseWebTest):
